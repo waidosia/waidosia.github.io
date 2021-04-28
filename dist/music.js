6f800a7ac3c0ce7cc9f0790f011dbceee29cc4c4
eddy <2578052882@qq.com>
2021-04-29 00:16:43 +0800

    Site updated: 2021-04-29 00:16:43

--- dist/music.js
+++ dist/music.js
@@ -7,22 +7,25 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: '花海',
+            name: '多幸运',
+            artist: '韩安旭',
+            url: 'http://t.hunlipic.com/?dxy ',
+            cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000008wVjT0MSG3T_1.jpg?max_age=2592000',
+            lrc: '多幸运-Vk.lrc'
+        },
+        {
+            name: '告白气球',
             artist: '周杰伦',
-            url: 'http://www.ytmp3.cn/down/73979.mp3',
-            cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002Neh8l0uciQZ_1.jpg?max_age=2592000',
+            url: 'http://t.hunlipic.com/?gbqq',
+            cover: 'https://bkimg.cdn.bcebos.com/pic/a8014c086e061d950e4f0d4c76f40ad162d9cabc?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UyNzI=,g_7,xp_5,yp_5/format,f_auto',
+            lrc: '告白气球-周杰伦.lrc'
         },
         {
-            name: '十一年',
-            artist: '邱永传',
-            url: 'https://m10.music.126.net/20210429001102/3c97b55748362f51b617ca549901d474/ymusic/7104/8547/3e3c/27dfbd02d15d68648222eb32f6331e64.mp3',
-            cover: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fweiyinyue.music.sina.com.cn%2Fwpp_cover%2F100044327_300300.jpg&refer=http%3A%2F%2Fweiyinyue.music.sina.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622197367&t=4c64f5957b4811251ca4a1f0bf4efaff',
-        },
-        {
-            name: '夜空中最亮的星',
-            artist: '张杰',
-            url: 'http://www.ytmp3.cn/down/32416.mp3',
-            cover: 'https://img0.baidu.com/it/u=3703288868,4180590989&fm=26&fmt=auto&gp=0.jpg',
+            name: '爱很简单',
+            artist: '陶喆',
+            url: 'http://t.hunlipic.com/?ahjd',
+            cover: 'https://bkimg.cdn.bcebos.com/pic/6159252dd42a2834a4f9ba9b51b5c9ea14cebfc7?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U4MA==,g_7,xp_5,yp_5/format,f_auto',
+            lrc:'陶喆-爱很简单.lrc'
         }
     ]
 });
