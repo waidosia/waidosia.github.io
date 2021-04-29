8be668b30489ac39d5d942b266f90b47ff608060
eddy <2578052882@qq.com>
2021-04-29 10:06:41 +0800

    Site updated: 2021-04-29 10:06:39

--- dist/music.js
+++ dist/music.js
@@ -9,7 +9,7 @@ const ap = new APlayer({
         {
             name: '多幸运',
             artist: '韩安旭',
-            url: 'http://t.hunlipic.com/?dxy ',
+            url: '3548771471.mp3',
             cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000008wVjT0MSG3T_1.jpg?max_age=2592000',
             lrc: '多幸运-Vk.lrc'
         },
@@ -23,7 +23,7 @@ const ap = new APlayer({
         {
             name: '爱很简单',
             artist: '陶喆',
-            url: 'http://t.hunlipic.com/?ahjd',
+            url: 'http://win.web.ra01.sycdn.kuwo.cn/59e89720370d0358e75665e0c525f008/60898b1e/resource/n2/128/56/31/812853666.mp3',
             cover: 'https://bkimg.cdn.bcebos.com/pic/6159252dd42a2834a4f9ba9b51b5c9ea14cebfc7?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U4MA==,g_7,xp_5,yp_5/format,f_auto',
             lrc:'陶喆-爱很简单.lrc'
         }
