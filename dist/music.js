b50a746883d5f694f4e0e0286fb24c155d1b5a64
eddy <2578052882@qq.com>
2021-05-03 00:05:26 +0800

    Site updated: 2021-05-03 00:05:24

--- dist/music.js
+++ dist/music.js
@@ -7,25 +7,39 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: '多幸运',
-            artist: '韩安旭',
-            url: 'source/3548771471.mp3',
-            cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000008wVjT0MSG3T_1.jpg?max_age=2592000',
-            lrc: '多幸运-Vk.lrc'
+            name: '晴天',
+            artist: '周杰伦',
+            url: 'source/晴天.mp3',
+            cover: 'images/晴天.jpg',
+            lrc: 'lrc/晴天.lrc'
         },
         {
             name: '告白气球',
             artist: '周杰伦',
-            url: 'http://t.hunlipic.com/?gbqq',
-            cover: 'https://bkimg.cdn.bcebos.com/pic/a8014c086e061d950e4f0d4c76f40ad162d9cabc?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UyNzI=,g_7,xp_5,yp_5/format,f_auto',
-            lrc: '告白气球-周杰伦.lrc'
+            url: 'source/告白气球.mp3',
+            cover: 'images/晴天.jpg',
+            lrc: 'lrc/告白气球.lrc'
         },
         {
-            name: '爱很简单',
-            artist: '陶喆',
-            url: 'http://win.web.ra01.sycdn.kuwo.cn/59e89720370d0358e75665e0c525f008/60898b1e/resource/n2/128/56/31/812853666.mp3',
-            cover: 'https://bkimg.cdn.bcebos.com/pic/6159252dd42a2834a4f9ba9b51b5c9ea14cebfc7?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U4MA==,g_7,xp_5,yp_5/format,f_auto',
-            lrc:'陶喆-爱很简单.lrc'
+            name: '不染',
+            artist: '毛不易',
+            url: 'source/不染.mp3',
+            cover: 'images/不染.jpg',
+            lrc:'lrc/不染.lrc'
+        },
+        {
+            name: '十一年',
+            artist: '邱永传',
+            url: 'source/十一年.mp3',
+            cover: 'images/十一年j.jpg',
+            lrc:'lrc/十一年.lrc'
+        },
+        {
+            name: '花海',
+            artist: '周杰伦',
+            url: 'source/花海.mp3',
+            cover: 'images/花海.jpg',
+            lrc:'lrc/花海.lrc'
         }
     ]
 });
